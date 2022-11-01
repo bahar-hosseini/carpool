58df6efd3ae3e009dc57266c369a3e0e3c61e19f
Bahar Hosseini <bahar.hosseini@gmail.com>
2022-11-01 14:29:08 -0400

    set up server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+// declarations
+require('dotenv').config()
+const { ENVIROMENT, PORT } = process.env
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+// middleware setup
+app.use(morgan(ENVIROMENT))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.json({ greetings: 'hello world' })
+})
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))

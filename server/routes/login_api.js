57e1225655fe77aac037c7b2e95cf93f3ea74c16
Bahar Hosseini <bahar.hosseini@gmail.com>
2022-11-07 18:41:02 -0500

    set up login frontend

--- server/routes/login_api.js
+++ server/routes/login_api.js
@@ -7,13 +7,15 @@ const queryUser = require('../db/queries/user_login');
 const generateToken = require('../utils/generate_token.js');
 
 router.post('/', (req, res) => {
-  // const { email, password } = req.body;
-  const email = 'bahar2.h@gmail.com';
+  const { email, password } = req.body;
+
   console.log('email', email);
+  console.log('password', password);
   queryUser
-    .getUser(email)
+    .getUser(email, password)
     .then((response) => {
       if (response) {
+        console.log(response);
         res.json({
           userId: response.id.toString(),
           email: response.email,
